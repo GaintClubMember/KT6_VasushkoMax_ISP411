959550b4a3b25f1425684fc31c215078d3a05c59
GaintMax <masesight@list.ru>
2024-10-29 11:17:03 +0300

    Initial Code

---
+++ KT_6_MaxVasushko_ISP_411/Classes/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace KT_6_MaxVasushko_ISP_411.Classes
+{
+    internal class Manager
+    {
+
+        public static Frame frameHelper {  get; set; }
+
+    }
+}
